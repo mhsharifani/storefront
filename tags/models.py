27ca9d1fa3c6ef1d122ccddf2a99d536f9b2b461
Hossein Sharifani <79485961+mhsharifani@users.noreply.github.com>
2023-04-06 02:45:32 +0430

    first

---
+++ tags/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+# Create your models here.
+
+class Tag(models.Model):
+
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    
+    #what tag applied to what object
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    #we need 2 things to identify any object (application) or any record in any tables(database)
+    # 1: Type (like products, videos, article etc.)
+    # 2: Id
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
